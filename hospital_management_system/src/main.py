57bf2d80e558549bbb404ec820740584c827bdc6
Misbah sajjad <154726572+misbahs10@users.noreply.github.com>
2025-09-26 14:47:39 +0500

    Update main.py

--- hospital_management_system/src/main.py
+++ hospital_management_system/src/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-
 from src.api.routes import router as patient_router
 
 app = FastAPI()
+app.include_router(patient_router, prefix="/patients", tags=["patients"])
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Hospital Management System"}
-
-app.include_router(patient_router, prefix="/patients", tags=["patients"])
 
 if __name__ == "__main__":
     import uvicorn
